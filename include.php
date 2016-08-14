c71bd2d986206020a159c3fc5dde8ac435006b33
Min <1131215955@qq.com>
2016-08-14 16:03:30 +0800

    包含mysql.func,common.func,admin.inc

--- include.php
+++ include.php
@@ -1,4 +1,5 @@
 <?php
+header("content-type:text/html;charset=utf-8");//设置字符编码类型,防止输出乱码
 session_start();//要删除其他文件的session_start,否则不绘制验证码
 define("ROOT",dirname(__FILE__));//定义当前文件所在目录为根路径
 //设置 include_path 配置选项
@@ -7,3 +8,5 @@ require_once 'string.func.php';
 require_once 'image.func.php';
 require_once 'configs.php';
 require_once 'mysql.func.php';
+require_once 'common.func.php';
+require_once 'admin.inc.php';
